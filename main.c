df4dd7061c0a3d39aef83d5c26dd0f2223c5cb37
Aditya Ghosh <adityaghosh5025@gmail.com>
2024-01-21 16:32:48 +0530

    added auto copying of data when front passes mid

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
+#include <assert.h>
 
 typedef struct {
   int *items;
@@ -14,7 +16,7 @@ typedef struct {
 void queue_push(Queue *queue, int item) {
   if (queue->rear >= queue->capacity) {
     queue->capacity = (queue->capacity == 0)? 1: queue->capacity*2;
-    queue->items = realloc(queue->items, queue->capacity);
+    queue->items = realloc(queue->items, queue->capacity*sizeof(queue->items[0]));
   }
   queue->items[queue->rear++] = item;
 }
@@ -24,7 +26,16 @@ bool queue_pop(Queue *queue, int *item) {
   if (queue->front == queue->rear) {
     return false;
   }
-  
+
+  size_t len = queue->rear - queue->front;
+  // may be optimised to only copy when len is too big
+  // only copies if the region behind front is larger than len
+  // making them non-overlapping which allows memcpy to be used
+  if (/*len > 16 && */ (queue->front - 1) > len) {
+    memcpy(queue->items, queue->items + queue->front, len*sizeof(queue->items[0]));
+    queue->front = 0;
+    queue->rear = len;
+  }
 
   *item = queue->items[queue->front++];
   return true;
@@ -38,6 +49,13 @@ int main() {
   queue_push(&queue, 82);
   queue_push(&queue, 75);
   queue_push(&queue, 1000);
+  queue_push(&queue, 1000);
+  queue_push(&queue, 1000);
+  queue_push(&queue, 1000);
+  queue_push(&queue, 1000);
+
+
+
 
   int x = 0;
   while(queue_pop(&queue, &x)) {
