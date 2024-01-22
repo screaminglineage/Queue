f8057a91289bc6c8b9804ef1fe069b73bf815c38
Aditya Ghosh <adityaghosh5025@gmail.com>
2024-01-22 19:31:36 +0530

    Adding Deque implementation

---
+++ Deque.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "Deque.h"
+
+#define MAX 8
+
+
+
+Deque deque_init() {
+    return (Deque){0};
+}
+
+void deque_del(Deque *d) {
+    d->len = 0;
+    d->front = 0;
+    d->rear = 0;
+}
+
+void deque_iter(Deque *deque) {
+    for (size_t i = deque->front; i != deque->rear; i = (i + 1) % MAX) {
+        printf("%d ", deque->items[i]);
+    }
+}
+
+
+bool deque_push_back(Deque *deque, int item) {
+    if (deque->len == MAX) {
+        return false;
+    }
+
+    deque->items[deque->rear] = item;
+    deque->rear = (deque->rear + 1) % MAX;
+    deque->len++;
+    return true;
+}
+
+bool deque_pop_front(Deque *deque, int *item) {
+    if (deque->len == 0) {
+        return false;
+    }
+
+    *item = deque->items[deque->front];
+    deque->front = (deque->front + 1) % MAX;
+    deque->len--;
+    return true;
+}
+
+
+
+
+
---
+++ Deque.h
@@ -0,0 +1,22 @@
+#ifndef DEQUE_H
+#define DEQUE_H
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#define MAX 8
+
+typedef struct {
+    int items[MAX];
+    size_t len;
+    size_t front;
+    size_t rear;    
+} Deque;
+
+Deque deque_init();
+void deque_del(Deque *d);
+bool deque_push_back(Deque *deque, int item);
+bool deque_pop_front(Deque *deque, int *item);
+void deque_iter(Deque *deque);
+
+#endif // DEQUE_H
