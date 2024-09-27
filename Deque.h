892d7812cb60fc4ec2951177f75af9e0287632fb
Aditya Ghosh <adityaghosh5025@gmail.com>
2024-09-27 13:32:27 +0530

    removed the rear element

--- Deque.h
+++ Deque.h
@@ -2,25 +2,33 @@
 #define DEQUE_H
 
 #include <stdbool.h>
-#include <stddef.h>
+#include <malloc.h>
 
 typedef struct {
     int *items;
     size_t len;
     size_t front;
-    size_t rear;    
     size_t capacity;
 } Deque;
 
-Deque deque_init();
-void deque_del(Deque *d);
+
+static inline Deque deque_init() {
+    return (Deque){0};
+}
+
+static inline void deque_del(Deque *d) {
+    free(d->items);
+    d->len = 0;
+    d->front = 0;
+    d->capacity = 0;
+}
+
 void deque_push_back(Deque *deque, int item);
 void deque_push_front(Deque *deque, int item);
 bool deque_pop_front(Deque *deque, int *item);
 bool deque_pop_back(Deque *deque, int *item);
 bool deque_peek_front(Deque *deque, int *item);
 bool deque_peek_back(Deque *deque, int *item);
-
 bool deque_iter(Deque *deque);
 
 #endif // DEQUE_H
